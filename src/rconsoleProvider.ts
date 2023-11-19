b3b3fa607b6dcc82a766ef1e328da31d20cdfc73
Oleg Klimov <omgtech@gmail.com>
2023-11-19 15:42:53 +0100

    shows command hints

--- src/rconsoleProvider.ts
+++ src/rconsoleProvider.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import * as vscode from 'vscode';
 import * as estate from "./estate";
+import * as rconsoleHints from "./rconsoleHints";
 
 
 class MyCommentAuthorInformation implements vscode.CommentAuthorInformation {
@@ -46,67 +47,105 @@ class MyComment implements vscode.Comment {
 //     }
 // }
 
-
-export async function super_test(editor: vscode.TextEditor)
+export function refact_console_close()
 {
     if (global.comment_disposables) {
         for (let d of global.comment_disposables) {
             d.dispose();
         }
     }
+    global.comment_disposables = [];
+}
 
+export async function open_refact_console_between_lines(editor: vscode.TextEditor)
+{
+    refact_console_close();
     let selected_range = editor.selection;
     let line0 = selected_range.start.line;
     let line1 = selected_range.end.line;
     let selected_lines_range = new vscode.Range(line0, 0, line1, 0);
-    // global.test_comments = vscode.comments.createCommentController("refactai-test", "RefactAI Test Comments");
     let cc = vscode.comments.createCommentController("refactai-test", "RefactAI Test Comments");
     cc.commentingRangeProvider = {
         provideCommentingRanges: (document: vscode.TextDocument) => {
             return [selected_lines_range];
-            // return [new vscode.Range(line0, 0, line1, 0)];
-            // return [new vscode.Range(0, 0, document.lineCount - 1, 0)];
         },
     };
     cc.reactionHandler = (comment: vscode.Comment, reaction: vscode.CommentReaction): Thenable<void> => {
         console.log("reactionHandler", comment, reaction);
         return Promise.resolve();
     };
-    vscode.workspace.onDidChangeTextDocument(e => {
-        if (e.document.uri.scheme !== "comment") {
-            return;
-        }
-        let text = e.document.getText();
-        console.log("onDidChangeTextDocument", text);
-    });
-    vscode.workspace.onDidCloseTextDocument(e => {
-        if (e.uri.scheme !== "comment") {
-            return;
-        }
-        console.log("onDidCloseTextDocument", e);
-    });
-
-    console.log("super_test");
-    let my_comments: vscode.Comment[] = [
-        // new MyComment("hello", vscode.CommentMode.Preview, new MyCommentAuthorInformation("me")),
-        // new MyComment("world", vscode.CommentMode.Preview, new MyCommentAuthorInformation("you")),
-    ];
+    let my_comments: vscode.Comment[] = [];
     let thread: vscode.CommentThread = cc.createCommentThread(
         editor.document.uri,
         new vscode.Range(selected_range.start.line, 0, selected_range.end.line, 0),
         my_comments,
     );
     thread.canReply = true;
-    thread.label = "Refact Command (F1)";
+    thread.label = "Refact Console (F1)";
     thread.collapsibleState = vscode.CommentThreadCollapsibleState.Expanded;
-    // console.log("thread", thread);
-    // global_thread = thread;
-    global.comment_disposables = [];
     global.comment_disposables.push(thread);
     global.comment_disposables.push(cc);
+
+    let hint_debounce: NodeJS.Timeout|undefined;
+    vscode.workspace.onDidChangeTextDocument(async e => {
+        if (e.document.uri.scheme !== "comment") {
+            return;
+        }
+        if (my_comments.length === 0) {
+            return;
+        }
+        let text = e.document.getText();
+        console.log("onDidChangeTextDocument", text);
+        // let y = e.document.fileName;  // "/commentinput-8d64259c-9607-4048-a9dc-a73f621e750d-1.md"
+        // let hints = rconsoleHints.get_hints([], text);
+        let [hint, author] = rconsoleHints.get_hints([], text, selected_range);
+        my_comments[0] = new MyComment(hint, vscode.CommentMode.Preview, new MyCommentAuthorInformation(author));
+        await vscode.commands.executeCommand('setContext', 'refactcx.runEsc', true);
+        if (hint_debounce) {
+            clearTimeout(hint_debounce);
+        }
+        hint_debounce = setTimeout(async () => {
+            // this is a heavy operation, changes the layout and lags the UI
+            thread.comments = my_comments;
+        }, 200);
+        if (text.includes("\n")) {
+            let first_line = text.split("\n")[0];
+            if (first_line.startsWith("/")) {
+                for (let cmd in rconsoleHints.commands_available) {
+                    if (first_line.startsWith("/" + cmd)) {
+                        activate_cmd(cmd);
+                        return;
+                    }
+                }
+            } else {
+                activate_chat(text);
+            }
+        }
+    });
+    vscode.workspace.onDidCloseTextDocument(e => {
+        if (e.uri.scheme !== "comment") {
+            return;
+        }
+        refact_console_close();
+    });
     await new Promise(resolve => setTimeout(resolve, 100));
-    my_comments.push(
-        new MyComment("<a href=\"x\">Make code shorter</a><br>\n<a href=\"x\">Comment each line</a>", vscode.CommentMode.Preview, new MyCommentAuthorInformation("Hints"))
-    );
-    thread.comments = my_comments;
+    function initial_message()
+    {
+        let [hint, author] = rconsoleHints.get_hints([], "", selected_range);
+        my_comments.push(new MyComment(hint, vscode.CommentMode.Preview, new MyCommentAuthorInformation(author)));
+        thread.comments = my_comments;
+    }
+    await vscode.commands.executeCommand('setContext', 'refactcx.runEsc', true);
+    initial_message();
+}
+
+function activate_cmd(cmd: string)
+{
+    console.log(`activate_cmd refactaicmd.cmd_${cmd}`);
+    vscode.commands.executeCommand("refactaicmd.cmd_" + cmd);
+}
+
+function activate_chat(input: string)
+{
+    console.log(`activate_chat question.length=${input.length}`);
 }
