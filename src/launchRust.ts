18c19385cc9bbc6c31a4d2163230e08529cacfa4
Oleg Klimov <omgtech@gmail.com>
2023-11-29 12:07:47 +0100

    test publish

--- src/launchRust.ts
+++ src/launchRust.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import * as vscode from 'vscode';
-import * as mod_child_process from 'child_process';
 import * as fetchH2 from 'fetch-h2';
 import * as userLogin from './userLogin';
 import { join } from 'path';
