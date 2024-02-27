460bf660a31da0caf8f6c51f7bef586dbd977552
Viktoriia <vika.nakoryk8@gmail.com>
2024-02-27 02:02:27 +0100

    remove unneccesary code, add type for promise in statTab

--- src/fetchAPI.ts
+++ src/fetchAPI.ts
@@ -662,7 +662,8 @@ export async function getAtCommandPreview(query: string): Promise<ChatContextFil
 
       return jsonMessages;
 }
-export async function get_statistic_data(): Promise<any> {
+
+export async function get_statistic_data(): Promise<{ data: string }> {
     let url = rust_url("/v1/get-dashboard-plots");
 
     if (!url) {
