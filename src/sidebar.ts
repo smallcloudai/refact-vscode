b15721a7e06b5a1278a9d57fd386d093ca6f25f6
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-03-01 18:42:43 +0100

    wip(menu) : add chat + hot. key to menu, and fix an issue with the short-cut not opening

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -75,6 +75,8 @@ export class PanelWebview implements vscode.WebviewViewProvider {
     public statistic: statisticTab.StatisticTab | null = null;
     public chatHistoryProvider: ChatHistoryProvider|undefined;
 
+    public static readonly viewType = "refactai-toolbox";
+
     constructor(private readonly _context: any) {
         this.chatHistoryProvider = undefined;
         this.address = "";
