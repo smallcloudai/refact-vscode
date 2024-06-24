ceb7d80af4419645630a9a37936d99c5b30b2966
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-06-24 17:34:21 +0200

    fix(handleChatQuestion): make tools parameter optional

--- src/chatTab.ts
+++ src/chatTab.ts
@@ -404,7 +404,7 @@ export class ChatTab {
         // messages: [string, string][];
         messages: ChatMessages;
         attach_file?: boolean;
-        tools: ToolCommand[] | null;
+        tools?: ToolCommand[] | null;
     }): Promise<void> {
         this.web_panel.webview.postMessage({type: EVENT_NAMES_TO_CHAT.SET_DISABLE_CHAT, payload: { id, disable: true }});
         // const file = attach_file && this.getActiveFileInfo();
