cb2861b9c0a8fe028aa1b8203f7f2535e4c4f69b
Marc McIntosh <marcmcintosh1987@gmail.com>
2024-08-21 18:04:15 +0200

    comment out new old code

--- src/chatTab.ts
+++ src/chatTab.ts
@@ -651,65 +651,65 @@ export class ChatTab {
         // }
     }
 
-    async handleOpenFile(file: {file_name:string, line?: number}) {
-        const uri = vscode.Uri.file(file.file_name);
-        const document = await vscode.workspace.openTextDocument(uri);
-        if(file.line !== undefined) {
-            const position = new vscode.Position(file.line ?? 0, 0);
-            const editor = await vscode.window.showTextDocument(document);
-            const range = new vscode.Range(position, position);
-            editor.revealRange(range);
-        }
-    }
+    // async handleOpenFile(file: {file_name:string, line?: number}) {
+    //     const uri = vscode.Uri.file(file.file_name);
+    //     const document = await vscode.workspace.openTextDocument(uri);
+    //     if(file.line !== undefined) {
+    //         const position = new vscode.Position(file.line ?? 0, 0);
+    //         const editor = await vscode.window.showTextDocument(document);
+    //         const range = new vscode.Range(position, position);
+    //         editor.revealRange(range);
+    //     }
+    // }
 
-    async handleDiffOperation(id: string, diff_id: string, chunks: DiffChunk[], to_apply: boolean[]) {
-        fetchAPI.send_chunks_to_diff_apply(chunks, to_apply).then((res) => {
-            const action: RecieveDiffOpperationResult = {
-                type: EVENT_NAMES_TO_CHAT.RECIEVE_DIFF_OPPERATION_RESULT,
-                payload: {
-                    id: id,
-                    diff_id,
-                    fuzzy_results: res.fuzzy_results,
-                    state: res.state,
-                }
-            };
+    // async handleDiffOperation(id: string, diff_id: string, chunks: DiffChunk[], to_apply: boolean[]) {
+    //     fetchAPI.send_chunks_to_diff_apply(chunks, to_apply).then((res) => {
+    //         const action: RecieveDiffOpperationResult = {
+    //             type: EVENT_NAMES_TO_CHAT.RECIEVE_DIFF_OPPERATION_RESULT,
+    //             payload: {
+    //                 id: id,
+    //                 diff_id,
+    //                 fuzzy_results: res.fuzzy_results,
+    //                 state: res.state,
+    //             }
+    //         };
 
-            this.web_panel.webview.postMessage(action);
-        }).catch((err: string) => {
-            const action: RecieveDiffOpperationError = {
-                type: EVENT_NAMES_TO_CHAT.RECIEVE_DIFF_OPPERATION_ERROR,
-                payload: {
-                  id,
-                  diff_id,
-                  reason: err
-                },
-              };
-            this.web_panel.webview.postMessage(action);
-        });
-    }
+    //         this.web_panel.webview.postMessage(action);
+    //     }).catch((err: string) => {
+    //         const action: RecieveDiffOpperationError = {
+    //             type: EVENT_NAMES_TO_CHAT.RECIEVE_DIFF_OPPERATION_ERROR,
+    //             payload: {
+    //               id,
+    //               diff_id,
+    //               reason: err
+    //             },
+    //           };
+    //         this.web_panel.webview.postMessage(action);
+    //     });
+    // }
 
-    async handleAppliedChunksRequest(id: string, diff_id: string, chunks: DiffChunk[]) {
-        fetchAPI.get_diff_state(chunks).then((diff_state) => {
-            const action: RecieveDiffAppliedChunks = {
-                type: EVENT_NAMES_TO_CHAT.RECIEVE_DIFF_APPLIED_CHUNKS,
-                payload: {
-                    id: id,
-                    diff_id,
-                    applied_chunks: diff_state.state,
-                    can_apply: diff_state.can_apply,
-                }
-            };
+    // async handleAppliedChunksRequest(id: string, diff_id: string, chunks: DiffChunk[]) {
+    //     fetchAPI.get_diff_state(chunks).then((diff_state) => {
+    //         const action: RecieveDiffAppliedChunks = {
+    //             type: EVENT_NAMES_TO_CHAT.RECIEVE_DIFF_APPLIED_CHUNKS,
+    //             payload: {
+    //                 id: id,
+    //                 diff_id,
+    //                 applied_chunks: diff_state.state,
+    //                 can_apply: diff_state.can_apply,
+    //             }
+    //         };
 
-            this.web_panel.webview.postMessage(action);
-        }).catch((err: string) => {
-            const action: RecieveDiffAppliedChunksError = {
-              type: EVENT_NAMES_TO_CHAT.RECIEVE_DIFF_APPLIED_CHUNKS_ERROR,
-              payload: { id, diff_id, reason: err },
-            };
+    //         this.web_panel.webview.postMessage(action);
+    //     }).catch((err: string) => {
+    //         const action: RecieveDiffAppliedChunksError = {
+    //           type: EVENT_NAMES_TO_CHAT.RECIEVE_DIFF_APPLIED_CHUNKS_ERROR,
+    //           payload: { id, diff_id, reason: err },
+    //         };
 
-            this.web_panel.webview.postMessage(action);
-        });
-    }
+    //         this.web_panel.webview.postMessage(action);
+    //     });
+    // }
 
     async handleOpenSettings() {
         return vscode.commands.executeCommand("workbench.action.openSettings", "refactai");
