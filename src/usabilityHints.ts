251ac7719c9e2f5a0973b483f8e0113c520aaf6e
alashchev17 <andrew.lashchev15@gmail.com>
2025-05-02 17:00:33 +0200

    feat: receiving and showing login message with links

--- src/usabilityHints.ts
+++ src/usabilityHints.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+import { toPascalCase } from 'refact-chat-js/dist/events';
 import * as vscode from 'vscode';
 
+const HTML_TAG_A_REGULAR_EXPRESSION = /<a\s+href="([^"]+)">([^<]+)<\/a>/i;
 
 export async function show_message_from_server(kind_of_message: string, msg: string)
 {
@@ -11,6 +13,7 @@ export async function show_message_from_server(kind_of_message: string, msg: str
     }
     let context = context_ as vscode.ExtensionContext;
     let already_seen = context.globalState.get<string>(`refactai.servermsg${kind_of_message}`);
+    
     if (already_seen === undefined) {
         already_seen = "";
     }
@@ -20,6 +23,48 @@ export async function show_message_from_server(kind_of_message: string, msg: str
     if (msg === "") {
         return false;
     }
+
+    const message_match_link = msg.match(HTML_TAG_A_REGULAR_EXPRESSION);
+    
+    let message_text = msg;
+    let link_label: string | undefined;
+    let link_href: string | undefined;
+
+    if (message_match_link) {
+        link_href = message_match_link[1];
+        link_label = message_match_link[2];
+        message_text = msg.replace(HTML_TAG_A_REGULAR_EXPRESSION, link_label);
+    }
+
+    if (link_href && link_label) {
+        const button_label = toPascalCase(link_label);
+        vscode.window.showInformationMessage(
+            message_text,
+            button_label
+        ).then((selection) => {
+            if (selection === button_label && link_href) {
+                try {
+                    const uri = vscode.Uri.parse(link_href, true);
+                    vscode.env.openExternal(uri)
+                        .then(
+                            success => {
+                                if (!success) {
+                                    vscode.window.showErrorMessage("Failed to open URL");
+                                }
+                            },
+                            error => {
+                                vscode.window.showErrorMessage(`Failed to open URL: ${error}`);
+                            }
+                        );
+                } catch (error) {
+                    console.error(error);
+                    vscode.window.showErrorMessage(`Failed to open URL: ${error}`);
+                }
+            }
+        });
+    } else {
+        vscode.window.showInformationMessage(msg);
+    }
+
     await context.globalState.update(`refactai.servermsg${kind_of_message}`, msg);
-    let selection = await vscode.window.showInformationMessage(msg, "OK");
 }
